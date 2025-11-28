c21193dc624b3e8dfc22d4fb93863cd1a2612ef0
Dahep <d.herrmann.p@gmail.com>
2025-11-28 17:42:29 -0300

    add steam setup to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source scripts/setup-ente.sh
 source scripts/setup-bashsimplecurses.sh
+source scripts/setup-steam.sh
 source scripts/setup-executables.sh
 stow --adopt . || exit 1
