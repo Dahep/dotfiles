be049209e20dbe4703645bcf574bb7a6717b164f
Dahep <d.herrmann.p@gmail.com>
2025-11-28 17:38:25 -0300

    change simple curses permissions

--- scripts/setup-bashsimplecurses.sh
+++ scripts/setup-bashsimplecurses.sh
@@ -3,4 +3,5 @@
 if [ ! -d ~/.local/lib ]; then
   mkdir -p ~/.local/lib
 fi
-make -C dependencies/simplecurses install PREFIX=~/.local
+make -C dependencies/simplecurses install PREFIX=~/.local -s
+chmod +x ~/.local/lib/simple_curses.sh
