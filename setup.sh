d45fa9bc88f938168b3bf905442b42abda2b8c2d
Dahep <d.herrmann.p@gmail.com>
2025-12-01 21:18:40 -0300

    "add get_imgs utility for downloading and printing bulk imgs"

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 source scripts/setup-ente.sh
 source scripts/setup-bashsimplecurses.sh
 source scripts/setup-steam.sh
+source scripts/setup-get-imgs.sh
 source scripts/setup-executables.sh
 stow --adopt . || exit 1
